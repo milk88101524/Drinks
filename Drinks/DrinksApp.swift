e997d6ec5d49e1e5d47b70a6b5eab61a85313991
Han <milk88101524@gmail.com>
2024-09-13 14:57:46 +0800

    Initial Commit

---
+++ Drinks/DrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksApp.swift
+//  Drinks
+//
+//  Created by Han on 2024/9/13.
+//
+
+import SwiftUI
+
+@main
+struct DrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
